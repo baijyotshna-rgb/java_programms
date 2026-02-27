72848c49c6a13d772a4aeca95a6cb614a91f55a7
baijyotshna-rgb <baijyotshna@gmail.com>
2026-02-27 08:15:25 +0530

    Add files via upload

---
+++ WelcomeUser.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class WelcomeUser
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+System.out.println("enter user name");
+String name=Sc.nextLine();
+System.out.println("Welcome"+name);
+}
+}
