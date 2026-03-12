7a0bfa6d88de18df3aa332e0ca37da565392af40
Bai Jyothsna <baijyotshna@gmail.com>
2026-03-12 19:24:20 +0530

    Add files via upload

---
+++ InstanceVariable.java
@@ -0,0 +1,9 @@
+class InstanceVariable
+{
+int a=6;
+public static void main(String args[])
+{
+InstanceVariable obj=new InstanceVariable();
+System.out.println("Instance variable value:" + obj.a);
+}
+}
