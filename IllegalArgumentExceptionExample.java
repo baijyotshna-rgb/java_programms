cb6922d1ac9b1e89d119ce33aa0de26053a2ef6e
Bai Jyothsna <baijyotshna@gmail.com>
2026-03-27 21:08:33 +0530

    Add files via upload

---
+++ IllegalArgumentExceptionExample.java
@@ -0,0 +1,12 @@
+class  IllegalArgumentExceptionExample{
+public static void main(String args[])
+{
+Thread t=new Thread();
+try{
+t.setPriority(15);
+}catch( IllegalArgumentException e)
+{
+System.out.println("invalid thread priority");
+}
+}
+}
