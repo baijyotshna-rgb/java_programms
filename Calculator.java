b099944c54b665daf52d654ef5be2776abd9c12f
Bai Jyothsna <baijyotshna@gmail.com>
2026-08-05 19:59:00 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Calculator {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+
+        if (b != 0)
+            System.out.println("Division = " + (a / b));
+        else
+            System.out.println("Division by zero not allowed.");
+
+        sc.close();
+    }
+}
